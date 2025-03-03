73923d18b314a410399d135f176147f00bc48a55
Ralph Nikko Predas <142536413+ralphpre24@users.noreply.github.com>
2025-03-03 08:02:51 +0800

    Telco.java

--- Telco.java
+++ Telco.java
@@ -1,1 +1,40 @@
+public class Telco implements TelcoSubscription {
 
+    private String telcoName;
+    private double promoPrice;
+    private int dataAllowance;
+    private boolean unliCallText;
+
+    public Telco(int dataAllowance, double promoPrice, String telcoName, boolean unliCallText) {
+        this.telcoName = telcoName;
+        this.promoPrice = promoPrice;
+        this.dataAllowance = dataAllowance;
+        this.unliCallText = unliCallText;
+    }
+
+    @Override
+    public String accept(UsagePromo usagePromo) {
+        return usagePromo.showAllowance(telcoName, promoPrice);
+    }
+
+    @Override
+    public String accept(UnliCallsTextOffer unliCallsTextOffer) {
+        return unliCallsTextOffer.showUnliCallsTextOffer(telcoName, unliCallText);
+    }
+
+    public String getTelcoName() {
+        return telcoName;
+    }
+
+    public double getPromoPrice() {
+        return promoPrice;
+    }
+
+    public int getDataAllowance() {
+        return dataAllowance;
+    }
+
+    public boolean isUnliCallText() {
+        return unliCallText;
+    }
+}
